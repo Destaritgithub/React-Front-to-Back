6503701ee8dad2552b079b13688e623560f08e55
Destaritgithub <alexdesse@gmail.com>
2023-04-20 00:48:17 -0700

    first commit check

---
+++ feedback/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  const title = 'Blog post';
+  const body = 'This is my blog post';
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+}
+export default App;
